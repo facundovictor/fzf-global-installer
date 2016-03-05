036c004e4d5d2ec391102842d1cf09d4f3bb8d12
Facundo Victor <facundovt@gmail.com>
2016-03-05 02:17:36 -0300

    Add function helpers
    Add copy_config_files, set_install_dir, install_fzf and git_clone.

--- installer.sh
+++ installer.sh
@@ -1,9 +1,29 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 # Install directory
-INSTALL_TARGET=/opt/fzf
+INSTALL_DIR=/opt/fzf
 
-mkdir -p $INSTALL_TARGET
+mkdir -p $INSTALL_DIR
 
+function git_clone () {
+    git clone --depth 1 https://github.com/junegunn/fzf.git $INSTALL_DIR
+}
+
+function install_fzf () {
+    $INSTALL_DIR/install --no-update-rc
+}
+
+function set_install_dir () {
+    sed -i.back "s,<INSTALL_DIR>,${INSTALL_DIR},g" etc/fzf/.fzf.bash 
+    sed -i.back "s,<INSTALL_DIR>,${INSTALL_DIR},g" etc/fzf/.fzf.zsh 
+}
+
+function copy_config_files () {
+    mkdir -p /etc/fzf
+    cp etc/fzf/.fzf.bash   /etc/fzf/.fzf.bash
+    cp etc/fzf/.fzf.zsh    /etc/fzf/.fzf.zsh
+    cp etc/fzf/.fzf.config /etc/fzf/.fzf.config
+    cp profile.d/fzf.sh    /etc/profile.d/fzf.sh
+}
 
 
